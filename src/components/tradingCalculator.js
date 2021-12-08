7a158f5aec944e0c2e1eaeabdb718d9595878e42
arwildo <arwildo@gmail.com>
2021-12-08 15:39:45 +0900

    add checks for SL error on buy/sell btn switch

--- src/components/tradingCalculator.js
+++ src/components/tradingCalculator.js
@@ -47,6 +47,24 @@ class TradingCalculator extends React.Component {
   // Direction
   handleChangeDirection(event) {
     event.preventDefault();
+    
+    // Check SL price error
+    if (this.state.direction == "long") {
+      if (this.state.stoploss > this.state.price) {
+        this.setState({hasError : false});
+      } else {
+        this.setState({hasError : true});
+        this.setState({errorInfo : "For sell trade stop loss must be lower than entry price."});
+      }
+    } else {
+      if (this.state.stoploss < this.state.price) {
+        this.setState({hasError : false});
+      } else {
+        this.setState({hasError : true});
+        this.setState({errorInfo : "For buy trade stop loss must be higher than entry price."});
+      }
+    }
+
     this.setState({direction: event.target.value});
   }
   // Price
